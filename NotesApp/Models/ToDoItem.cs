bf51fc0030173baccb105ce06e44f371bc01299d
Anastasija Lagodzinska <a.lagodzinska@outlook.com>
2022-12-05 15:43:08 -0800

    Smalll changes

--- NotesApp/Models/ToDoItem.cs
+++ NotesApp/Models/ToDoItem.cs
@@ -5,12 +5,13 @@ namespace NotesApp.Models
     public class ToDoItem
     {
         public int Id { get; set; }
-
+        [Display(Name = "Task")]
         public string Content { get; set; }
-
+        [Display(Name = "Done")]
         public bool IsDone { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Date of creation")]
         public DateTime CreationDate { get; set; }
     }
 }
