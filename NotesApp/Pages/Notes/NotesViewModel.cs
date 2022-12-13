f67c088fc66b567f79307b4b2985023d471b039f
Anastasija Lagodzinska <a.lagodzinska@outlook.com>
2022-12-12 19:01:11 -0800

    Latest Changes

---
+++ NotesApp/Pages/Notes/NotesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using NotesApp.Models;
+using NuGet.DependencyResolver;
+
+namespace NotesApp.Pages.Notes
+{
+    public class NotesViewModel
+    {
+        public IEnumerable<ToDoNote> ToDoNotes { get; set; }
+        public IEnumerable<TextNote> TextNotes { get; set; }
+    }
+}
